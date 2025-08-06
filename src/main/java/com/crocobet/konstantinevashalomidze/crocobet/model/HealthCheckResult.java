6511048c4ac5ee7b0ee6b1cfc996ed8630faec5d
KonstantineVasahalomize <vashalomidzekonstantine@gmail.com>
2025-08-06 22:27:10 +0400

    refactor(HealthCheckResult): add new dependency <lombok>, change HealthCheckResult class from record to class as builder pattern was necessary

--- src/main/java/com/crocobet/konstantinevashalomidze/crocobet/model/HealthCheckResult.java
+++ src/main/java/com/crocobet/konstantinevashalomidze/crocobet/model/HealthCheckResult.java
@@ -1,13 +1,34 @@
 package com.crocobet.konstantinevashalomidze.crocobet.model;
 
+import lombok.Builder;
+import lombok.Data;
 import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
 
-public record HealthCheckResult(String endpoint, boolean isHealthy, long responseTimeMs, HttpStatus statusCode, String errorMessage, LocalDateTime timestamp, String responseBody) {
+@Data
+@Builder
+public class HealthCheckResult {
+    private String endpoint;
+    private boolean isHealthy;
+    private long responseTimeMs;
+    private HttpStatus statusCode;
+    private String errorMessage;
+    private LocalDateTime timestamp;
+    private String responseBody;
 
-    public HealthCheckResult {
+    public HealthCheckResult() {
         timestamp = LocalDateTime.now();
+    }
+
+    public HealthCheckResult(String endpoint, boolean isHealthy, long responseTimeMs, HttpStatus statusCode, String errorMessage, String responseBody) {
+        this();
+        this.endpoint = endpoint;
+        this.isHealthy = isHealthy;
+        this.responseTimeMs = responseTimeMs;
+        this.statusCode = statusCode;
+        this.errorMessage = errorMessage;
+        this.responseBody = responseBody;
     }
 
 }
---
+++ src/main/java/com/crocobet/konstantinevashalomidze/crocobet/service/HealthCheckService.java
@@ -0,0 +1,24 @@
+package com.crocobet.konstantinevashalomidze.crocobet.service;
+
+
+import com.crocobet.konstantinevashalomidze.crocobet.model.HealthCheckResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.client.RestTemplate;
+
+
+public class HealthCheckService {
+    private static final Logger logger = LoggerFactory.getLogger(HealthCheckService.class);
+
+    private final RestTemplate restTemplate;
+
+    public HealthCheckService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public HealthCheckResult checkEndpoint(String url) {
+        HealthCheckResult result = new HealthCheckResult();
+    }
+
+
+}
