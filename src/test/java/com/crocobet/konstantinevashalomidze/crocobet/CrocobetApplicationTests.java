773811e33223f9c35438bfdc44bf87208fdd9329
KonstantineVasahalomize <vashalomidzekonstantine@gmail.com>
2025-08-06 21:07:56 +0400

    feat: fresh start, with <spring-boot-starter-web> dependency

---
+++ src/test/java/com/crocobet/konstantinevashalomidze/crocobet/CrocobetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crocobet.konstantinevashalomidze.crocobet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrocobetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
